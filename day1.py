1c8db8d143a569cfc2c5192c5a8b239d0afb7c08
henrik <hnytun@gmail.com>
2022-12-01 11:27:35 +0100

    day 1

---
+++ day1.py
@@ -0,0 +1,19 @@
+file1 = open('inputs/day1.txt', 'r')
+meals = file1.read().split('\n')
+
+elfCalories=list()
+
+cals=0
+for meal in meals:
+    if(meal != ''):
+        cals += int(meal)
+    else:
+        elfCalories.append(cals)
+        cals=0
+
+
+print("part one:")
+print(max(elfCalories))
+
+print("part two:")
+print(sum(sorted(elfCalories, reverse=True)[:3]))
